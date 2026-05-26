403493383bd5836a9c9b4286749202f91edac8ce
Mehdi Abdollahi <mehdi7007@gmail.com>
2026-05-26 13:40:14 +0330

    create agency model and agency_address model

--- agencies/admin.py
+++ agencies/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Agency , AgencyAddress
 
-# Register your models here.
+
+class AgencyAddressInLine(admin.StackedInline):
+    model = AgencyAddress
+    can_delete = False
+    verbose_name = "agency_address"
+    verbose_name_plural = "agency_addresses"
+    fk_name = "agency"
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    inlines = [AgencyAddressInLine]
+    """showing fields in main menu"""
+    list_display = ('agent' , 'name' , 'license_number' , 'is_verified')
+
+    """filtering items and clicking"""
+    search_fields = ('name' , 'license_number' , 'is_verified')
+    list_filter = ('name' , 'license_number' , 'business_phone' , 'agent__phone_number')
+
+    """ordering by """
+    ordering = ('-created',)
+
+    @admin.display(description='province')
+    def province_from_address(self, obj):
+        address = getattr(obj, 'address', None)
+        return address.province if address else "-"
--- agencies/models.py
+++ agencies/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Agency(models.Model):
+    """agent"""
+    agent = models.OneToOneField(User , on_delete=models.CASCADE , related_name='agency')
+    """agency Information"""
+    name = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=155)
+    business_phone = models.CharField(max_length=11)
+    description = models.TextField()
+    """verify"""
+    is_verified = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "agency"
+        verbose_name_plural = "agencies"
+
+
+class AgencyAddress(models.Model):
+    """agency"""
+    agency = models.OneToOneField(Agency , on_delete=models.CASCADE , related_name='address')
+    """address Information step by step"""
+    province = models.CharField(max_length=122)
+    city = models.CharField(max_length=122)
+    exact_address = models.TextField()
+
+    class Meta:
+        verbose_name = "agency_address"
+        verbose_name_plural = "agency_addresses"
